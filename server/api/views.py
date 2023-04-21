6953ba2f9080ed67aa786ca4aa3b6a032f5da0f1
dorothysyu <yu.dor@husky.neu.edu>
2023-04-21 16:03:14 -0400

    changed data structure of albums_info to be list of dicts

--- server/api/views.py
+++ server/api/views.py
@@ -1,7 +1,18 @@
+import json
 from django.shortcuts import render
-from rest_framework import generics
-from django.http import HttpResponse
+from rest_framework import viewsets
+from rest_framework.response import Response
+from django.http import HttpResponse, JsonResponse
+from api.models import Item
+from server.server import Spotify
+    
+def index(request):
+    return HttpResponse("hellooo")
 
-# Create your views here.
-def main(request):
-    return HttpResponse("hello")
+def spotify(request):
+    sp = Spotify()
+    return JsonResponse(sp.albums_info, safe=False)
+
+# def draw_spotify(request):
+#     sp = Spotify()
+#     return HttpResponse(render_gallery(sp.albums_info))
--- server/server/server.py
+++ server/server/server.py
@@ -16,31 +16,42 @@ class Spotify():
         self.spotify_object = spotipy.Spotify(auth=oauth_object.get_access_token())
         self.top_tracks = self.spotify_object.current_user_top_tracks(time_range="short_term", limit=50)['items']
         # List comprehension. For every JSON item in top_tracks, I only want the key value pair 'album'.
-        self.top_albums = [x['album'] for x in self.top_tracks]
+        self.top_albums = [track['album'] for track in self.top_tracks]
         """
-        albums_info attribute:
-        Key: album_id
-        Value: List
-            Index 0: integer representing frequency of album in user's top tracks.
-            Index 1: string representing URL of album cover.
+        albums_info:
+        list of dictionaries:
+            { 
+                'id': value,
+                'frequency': value,
+                'url': value
+                'ranking': value
+            }
         """
-        self.albums_info = {}   
+        self.albums_info = [] 
         self.set_albums_info()
 
     def set_albums_info(self):     
-        top_albums_length = 0
+        albums_length = 0
+        seen_ids = []
         for album in self.top_albums:
-            if top_albums_length is 50: break
             album_id = album['id']
-            if album_id not in self.albums_info.keys():
-                self.albums_info[album_id] = [1]
-                self.albums_info[album_id].append(album['images'][0]['url'])
+            if album_id not in seen_ids:
+                album_obj = {}
+                album_obj['id'] = album_id
+                album_obj['frequency'] = 1
+                album_obj['url'] = album['images'][0]['url']
+                album_obj['ranking'] = albums_length
+                albums_length += 1
+                self.albums_info.append(album_obj)
+                seen_ids.append(album_id)
             else:
-                self.albums_info[album_id][0] += 1
-            top_albums_length += 1
+                for dic in self.albums_info:
+                    if dic['id'] == album_id:
+                        dic['frequency'] += 1
+
         return self.albums_info
     
 if __name__ == "__main__":
     sp = Spotify()
-    with open('dump3.json', 'w') as dump:
+    with open('dump4.json', 'w') as dump:
         dump.write(json.dumps(sp.albums_info, sort_keys=False))
