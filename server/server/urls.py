7c7dd6aa136ac8f335e8875a2313c505cf67ecda
Amanda Escaba <amandaescaba@gmail.com>
2023-04-27 21:25:23 -0400

    deleted comments and import statement

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,18 +1,8 @@
-from django.conf.urls import include, url
-# from .views import GalleryView
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
 from rest_framework import routers
 from api import views
-
-# router = routers.DefaultRouter()
-# router.register(prefix=r'item', viewset=ItemViewSet, basename='item')
-
-# urlpatterns = [
-#     path(r'^api/', include(router.urls)) ,
-#     url(r'^admin/', admin.site.urls),
-# ]
 
 urlpatterns = [
     path('', views.index),
