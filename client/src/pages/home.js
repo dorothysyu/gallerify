c2b1d303c53217217912c3642dcf60e3542e9175
Amanda Escaba <amandaescaba@gmail.com>
2023-05-18 22:37:08 -0400

    gave frame beige bg; started styling landing page

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,8 +1,14 @@
+import frame from '../assets/frames/main_frame.png';
+
 const Home = () => {
     return (
-        <div id="home-frame" className="">
-            <h1>gallerify</h1>
-            <p>what does your music library gallery look like?</p>
+        <div id="home-frame">
+            <div id="home-contents" className="" style={{zIndex: 4}}>
+                <h1>gallerify</h1>
+                <p>what does your music library gallery look like?</p>
+                <button>test button</button>  
+            </div>
+            {/* <img src={frame}/> */}
         </div>
     );
 };
