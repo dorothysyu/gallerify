058a27f3c82171b1c26898077632752cc4124a3c
Amanda Escaba <amandaescaba@gmail.com>
2023-06-22 22:10:25 -0400

    made button component and tested styling with landing page styles

---
+++ client/src/components/button.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// https://devcamp.com/trails/comprehensive-react-development-tutorial/campsites/react-project-two-build-birthday-countdown-application/guides/how-to-create-functional-button-component-react
+
+const Button = () => {
+    return (
+        <a href="https://www.google.com/" target="_blank">
+            <button className="button">
+                gallerify me!
+            </button>
+        </a>
+    )
+}
+
+export default Button;
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,12 +1,12 @@
 import frame from '../assets/frames/main_frame.png';
-
+import Button from '../components/button';
 const Home = () => {
     return (
         <div id="home-frame">
-            <div id="home-contents" className="" style={{zIndex: 4}}>
+            <div id="home-contents" className="" style={{ zIndex: 4 }}>
                 <h1>gallerify</h1>
                 <p>what does your music library gallery look like?</p>
-                <button>test button</button>  
+                <Button />
             </div>
             {/* <img src={frame}/> */}
         </div>
